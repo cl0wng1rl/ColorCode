cbc4acb64984555fcd7287e720bd774e6b121432
Gabriel Barker <gabrielbarker@gmail.com>
2019-12-27 01:44:00 +0000

    Refactor Theme class

--- src/lib/ColorMind.ts
+++ src/lib/ColorMind.ts
@@ -1,22 +1,24 @@
 import { XMLHttpRequest } from "xmlhttprequest-ts";
 
-exports.getRandomColourPalette = function() {
-  return new Promise((resolve, reject) => {
-    var url = "http://colormind.io/api/";
-    var data = {
-      model: "default"
-    };
+export default class ColorMind {
+  static async getRandomColourPalette() {
+    return new Promise<string[]>((resolve, reject) => {
+      var url = "http://colormind.io/api/";
+      var data = {
+        model: "default"
+      };
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        return resolve(JSON.parse(xhr.responseText).result);
-      } else if (xhr.readyState === 4 && xhr.status !== 200) {
-        return reject("ERROR");
-      }
-    };
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          return resolve(JSON.parse(xhr.responseText).result);
+        } else if (xhr.readyState === 4 && xhr.status !== 200) {
+          return reject("ERROR");
+        }
+      };
 
-    xhr.open("POST", url, true);
-    xhr.send(JSON.stringify(data));
-  });
-};
+      xhr.open("POST", url, true);
+      xhr.send(JSON.stringify(data));
+    });
+  }
+}
