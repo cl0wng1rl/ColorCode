8d99a5e4cbba604117050b26332a517073e9ab74
Gabriel Barker <gabrielbarker@gmail.com>
2019-12-27 17:02:33 +0000

    Minor edits

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import Theme from "./lib/Theme";
+import ColorMind from "./lib/ColorMind";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "colorcode" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "colorcode" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("extension.generateTheme", () => {
+    // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    ColorMind.getRandomColourPalette().then(colorStrings => {
+      const settings = Theme.generateSettingsFromColorStrings(colorStrings);
+      console.log(settings);
+    });
+    // Display a message box to the user
+    vscode.window.showInformationMessage("New Theme Generated!");
+  });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
